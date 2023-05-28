106fc6e5719e16435668d315a8fa55cb2e9a478f
juamaya <marinamaya@hotmail.es>
2023-05-28 18:12:24 +0200

    cambios

--- src/componentes/Spinner.jsx
+++ src/componentes/Spinner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Spinner = () => {
     return (
         <div  className="  text-center mx-5">
-            <div className="spinner-border text-primary mx-5 " role="status">
+           {/*  <div className="spinner-border text-primary mx-5 " role="status">
                 <span className="visually-hidden">Loading...</span>
             </div>
             <div className="spinner-border text-secondary mx-5" role="status">
@@ -14,7 +14,7 @@ export const Spinner = () => {
             </div>
             <div className="spinner-border text-danger mx-5" role="status">
                 <span className="visually-hidden">Loading...</span>
-            </div>
+            </div> */}
             <div className="spinner-border text-warning mx-5" role="status">
                 <span className="visually-hidden">Loading...</span>
             </div>
